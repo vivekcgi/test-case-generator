e0029154a148942334f460a03839b1c940987e73
kishor <pawarkishor063@gmail.com>
2024-02-24 17:35:35 +0530

    updated flow

--- frontend/src/componensts/Header.jsx
+++ frontend/src/componensts/Header.jsx
@@ -1,12 +1,19 @@
 import { Link, NavLink } from "react-router-dom";
 const Header = () => {
+    const token= localStorage.getItem("token");
     return (
         <>
             <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-1">
                 <div className="container container-fluid">
-                    <Link className="navbar-brand" to="/">
-                       TestGeni
+                    {token?
+                    <Link className="navbar-brand" to="/home">
+                        TestGeni
                     </Link>
+                    :
+                    <Link className="navbar-brand" to="/">
+                        TestGeni
+                    </Link>
+                    }
                     <button
                         className="navbar-toggler"
                         type="button"
@@ -18,15 +25,15 @@ const Header = () => {
                     >
                         <span className="navbar-toggler-icon"></span>
                     </button>
-
+                    {token &&
                     <div
                         className="collapse navbar-collapse"
                         id="navbarCollapse"
                     >
-                        <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                       <ul className="navbar-nav me-auto mb-2 mb-md-0">
                             <li className="nav-item">
                                 <NavLink
-                                    to="/"
+                                    to="/home"
                                     className={({ isActive }) =>
                                         isActive
                                             ? "active  nav-link"
@@ -49,21 +56,25 @@ const Header = () => {
                                 </NavLink>
                             </li>
                         </ul>
+                        :
                         <ul className="navbar-nav mb-2 mb-md-0">
                             <li className="nav-item">
                                 <NavLink
-                                    to="/login"
+                                    onClick={()=>{
+                                        localStorage.removeItem("token")
+                                        localStorage.removeItem("setting")}}
                                     className={({ isActive }) =>
                                         isActive
                                             ? "active  nav-link"
                                             : "inactive  nav-link"
                                     }
                                 >
-                                    Login
+                                    Logout
                                 </NavLink>
                             </li>
                         </ul>
                     </div>
+}
                 </div>
             </nav>
         </>
